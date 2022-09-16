708f9bcd558f23ea004a3a05b2035a5eff7333a6
atmason90 <atmason90@gmail.com>
2022-09-16 01:08:20 -0700

    portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,64 @@
 import React from 'react'
 import './portfolio.css'
+// import portfolio project images
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'></section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'></div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="" className='btn' target='_blank'>Github</a>
+            <a href="" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'></div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="" className='btn' target='_blank'>Github</a>
+            <a href="" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'></div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="" className='btn' target='_blank'>Github</a>
+            <a href="" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'></div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="" className='btn' target='_blank'>Github</a>
+            <a href="" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'></div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="" className='btn' target='_blank'>Github</a>
+            <a href="" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'></div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="" className='btn' target='_blank'>Github</a>
+            <a href="" className='btn btn-primary' target='_blank'>Demo</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
