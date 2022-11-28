c60cb3fd939b8a9e87fe531147a39ae2f71cb946
atmason90 <atmason90@gmail.com>
2022-11-28 14:37:41 -0800

    add new portfolio project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './portfolio.css'
 // import portfolio project images
+import taskmanager from '../../assets/taskmanager.gif'
 import hungrysharks from '../../assets/hungrysharks.gif'
 import bookend from '../../assets/bookend.jpeg'
 import techblog from '../../assets/techblog.jpeg'
@@ -12,48 +13,55 @@ import ems from '../../assets/ems.jpeg'
 const data = [
   {
     id: 1,
+    image: taskmanager,
+    title: 'Task Manager',
+    github: 'https://github.com/atmason90/task-manager',
+    demo: 'https://drive.google.com/file/d/1537rwxcusjiyKSdBIf_pWVk4laDyaGDL/view?usp=sharing'
+  },
+  {
+    id: 2,
     image: hungrysharks,
     title: 'Hungry Sharks Card Game',
     github: 'https://github.com/atmason90/hungry-sharks',
     demo: 'https://hungryshark.herokuapp.com/'
   },
   {
-    id: 2,
+    id: 3,
     image: bookend,
     title: 'Bookend Social Media App',
     github: 'https://github.com/atmason90/bookend',
     demo: 'https://glacial-plateau-74113.herokuapp.com/'
   },
   {
-    id: 3,
+    id: 4,
     image: techblog,
     title: 'Tech Blog App',
     github: 'https://github.com/atmason90/tech-blog-app',
     demo: 'https://the-tech-blog-am.herokuapp.com/'
   },
   {
-    id: 4,
+    id: 5,
     image: texteditor,
     title: 'Text Editor Progressive Web App',
     github: 'https://github.com/atmason90/text-editor',
     demo: 'https://editmytext.herokuapp.com/'
   },
   {
-    id: 5,
+    id: 6,
     image: ecomm,
     title: 'E-Commerce Backend API',
     github: 'https://github.com/atmason90/e-comm-back-end',
     demo: 'https://drive.google.com/file/d/1KBD6tlB82vbEWURTLGDzOHshITIDAft2/view?usp=sharing'
   },
   {
-    id: 6,
+    id: 7,
     image: ems,
     title: 'Team Profile Generator',
     github: 'https://github.com/atmason90/team-profile-generator',
     demo: 'https://user-images.githubusercontent.com/99947655/165204617-ddd7d97b-8db4-4af3-b01e-8de7900dcdaa.mp4'
   },
   {
-    id: 7,
+    id: 8,
     image: socialnetwork,
     title: 'Social Network Backend API',
     github: 'https://github.com/atmason90/social-network-api',
